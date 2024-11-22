6b598de0063717400e7bd47907ceb180ed199c90
yunji <yunkim1346@gmail.com>
2024-11-22 15:19:09 +0900

    feat: Junit5 완성

--- src/main/java/com/placeHere/server/service/user/UserService.java
+++ src/main/java/com/placeHere/server/service/user/UserService.java
@@ -1,4 +1,8 @@
-package com.example.server.service.user;
+package com.placeHere.server.service.user;
+
+import com.placeHere.server.domain.User;
 
 public interface UserService {
+
+    public User getUser(String username) throws Exception;
 }
--- src/main/java/com/placeHere/server/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/placeHere/server/service/user/impl/UserServiceImpl.java
@@ -1,4 +1,23 @@
-package com.example.server.service.user.impl;
+package com.placeHere.server.service.user.impl;
 
-public class UserServiceImpl {
+
+import com.placeHere.server.dao.user.UserDao;
+import com.placeHere.server.domain.User;
+import com.placeHere.server.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service("userServiceImpl")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    @Qualifier("userDao")
+    UserDao userDao;
+
+    public User getUser(String username) throws Exception {
+        return null;
+    }
+
+
 }
