d6761157fbc75c7da8941dbe64970fe81f8d1ec0
afreet98 <afreet98@naver.com>
2024-11-27 16:05:46 +0900

    fix: 예약 시스템 컨트롤러, 테스트 오류 수정

--- src/main/java/com/placeHere/server/controller/reservation/ReservationController.java
+++ src/main/java/com/placeHere/server/controller/reservation/ReservationController.java
@@ -21,9 +21,6 @@ import java.util.List;
 public class ReservationController {
 
     @Autowired
-    @Qualifier("productServiceImpl")
-    private ProductService productService;
-    @Autowired
     private ReservationService reservationService;
 
     public ReservationController(){
@@ -56,10 +53,10 @@ public class ReservationController {
 
         // 조건 설정
         if (startDate != null) {
-            search.setStartDate(Date.valueOf(startDate)); // "yyyy-MM-dd" 형식 필요
+            search.setStartDate(startDate); // "yyyy-MM-dd" 형식 필요
         }
         if (endDate != null) {
-            search.setEndDate(Date.valueOf(endDate)); // "yyyy-MM-dd" 형식 필요
+            search.setEndDate(endDate); // "yyyy-MM-dd" 형식 필요
         }
         search.setSearchStatuses(searchStatuses); // 예약 상태 목록
 
