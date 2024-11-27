137d05c14901d3574af60716cd54acd8bc7fd380
peace1nmind <aont95@naver.com>
2024-11-27 10:45:44 +0900

    fix: JWT 관련 주석처리

--- src/main/java/com/placeHere/server/config/SecurityConfig.java
+++ src/main/java/com/placeHere/server/config/SecurityConfig.java
@@ -1,114 +1,114 @@
-package com.placeHere.server.config;
-
-import com.placeHere.server.jwt.JWTFilter;
-import com.placeHere.server.jwt.JWTUtil;
-import com.placeHere.server.jwt.LoginFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Collections;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final JWTUtil jwtUtil;
-
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
-        this.authenticationConfiguration = authenticationConfiguration;
-        this.jwtUtil = jwtUtil;
-    }
-
-    //AuthenticationManager Bean 등록
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-
-        return configuration.getAuthenticationManager();
-    }
-
-    // 비밀번호 암호화
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .cors(cors -> cors
-                        .configurationSource(new CorsConfigurationSource() {
-                            @Override
-                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                                CorsConfiguration configuration = new CorsConfiguration();
-
-                                // 허용할 서버포트
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                                // get, post, option 등
-                                configuration.setAllowedMethods(Collections.singletonList("*"));
-                                configuration.setAllowCredentials(true);
-                                configuration.setAllowedHeaders(Collections.singletonList("*"));
-                                configuration.setMaxAge(3600L);
-
-                                // 헤더에 Authorization 넘겨줄 수 있게 허용
-                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-                                return configuration;
-                            }
-                        }));
-
-        //csrf disable
-        // session을 stateless 상태로 관리
-        http
-                .csrf((auth) -> auth.disable());
-
-        //From 로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
-
-        //http basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
-
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        // 다른 경로에 대해서는 로그인한 사용자만 사용할 수 있다.
-                        .anyRequest().authenticated());
-
-        // 필터 추가
-        // 로그인 필터 앞에 필터를 넣어준다.
-        // 순서 : 넣어줄 필터, 어디 앞에 등록할건지
-        http
-                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
-
-        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
-        http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
-        //세션 설정
-        // stateless 상태인게 가장 중요
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-
-        return http.build();
-    }
-}
+//package com.placeHere.server.config;
+//
+//import com.placeHere.server.jwt.JWTFilter;
+//import com.placeHere.server.jwt.JWTUtil;
+//import com.placeHere.server.jwt.LoginFilter;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//
+//import java.util.Collections;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+//    private final AuthenticationConfiguration authenticationConfiguration;
+//    private final JWTUtil jwtUtil;
+//
+//    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+//        this.authenticationConfiguration = authenticationConfiguration;
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//    //AuthenticationManager Bean 등록
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//
+//        return configuration.getAuthenticationManager();
+//    }
+//
+//    // 비밀번호 암호화
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//                .cors(cors -> cors
+//                        .configurationSource(new CorsConfigurationSource() {
+//                            @Override
+//                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//                                CorsConfiguration configuration = new CorsConfiguration();
+//
+//                                // 허용할 서버포트
+//                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//                                // get, post, option 등
+//                                configuration.setAllowedMethods(Collections.singletonList("*"));
+//                                configuration.setAllowCredentials(true);
+//                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                                configuration.setMaxAge(3600L);
+//
+//                                // 헤더에 Authorization 넘겨줄 수 있게 허용
+//                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//
+//                                return configuration;
+//                            }
+//                        }));
+//
+//        //csrf disable
+//        // session을 stateless 상태로 관리
+//        http
+//                .csrf((auth) -> auth.disable());
+//
+//        //From 로그인 방식 disable
+//        http
+//                .formLogin((auth) -> auth.disable());
+//
+//        //http basic 인증 방식 disable
+//        http
+//                .httpBasic((auth) -> auth.disable());
+//
+//        //경로별 인가 작업
+//        http
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers("/login", "/", "/join", "/store/**").permitAll()
+//                        .requestMatchers("/admin").hasRole("ADMIN")
+//                        // 다른 경로에 대해서는 로그인한 사용자만 사용할 수 있다.
+//                        .anyRequest().authenticated());
+//
+//        // 필터 추가
+//        // 로그인 필터 앞에 필터를 넣어준다.
+//        // 순서 : 넣어줄 필터, 어디 앞에 등록할건지
+//        http
+//                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+//
+//        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+//        http
+//                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+//
+//        //세션 설정
+//        // stateless 상태인게 가장 중요
+//        http
+//                .sessionManagement((session) -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//
+//        return http.build();
+//    }
+//}
--- src/main/java/com/placeHere/server/domain/CustomUserDetails.java
+++ src/main/java/com/placeHere/server/domain/CustomUserDetails.java
@@ -1,74 +1,74 @@
-package com.placeHere.server.domain;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-public class CustomUserDetails implements UserDetails {
-
-    private final User user;
-
-    public CustomUserDetails ( User user ) {
-        this.user = user;
-    }
-
-
-    // ROLE 값 반환
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-
-        collection.add(new GrantedAuthority() {
-
-            @Override
-            public String getAuthority() {
-
-                return user.getRole();
-            }
-        });
-
-        return collection;
-    }
-
-    @Override
-    public String getPassword() {
-
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-
-        return user.getUsername();
-    }
-
-    // 계정이 만료 되었는지
-    @Override
-    public boolean isAccountNonExpired() {
-
-        return true;
-    }
-
-    // 계정이 잠겼는지
-    @Override
-    public boolean isAccountNonLocked() {
-
-        return true;
-    }
-
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-
-        return true;
-    }
-}
+//package com.placeHere.server.domain;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//
+//public class CustomUserDetails implements UserDetails {
+//
+//    private final User user;
+//
+//    public CustomUserDetails ( User user ) {
+//        this.user = user;
+//    }
+//
+//
+//    // ROLE 값 반환
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//
+//        Collection<GrantedAuthority> collection = new ArrayList<>();
+//
+//        collection.add(new GrantedAuthority() {
+//
+//            @Override
+//            public String getAuthority() {
+//
+//                return user.getRole();
+//            }
+//        });
+//
+//        return collection;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//
+//        return user.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//
+//        return user.getUsername();
+//    }
+//
+//    // 계정이 만료 되었는지
+//    @Override
+//    public boolean isAccountNonExpired() {
+//
+//        return true;
+//    }
+//
+//    // 계정이 잠겼는지
+//    @Override
+//    public boolean isAccountNonLocked() {
+//
+//        return true;
+//    }
+//
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//
+//        return true;
+//    }
+//}
