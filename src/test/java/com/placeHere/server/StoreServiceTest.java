e074910f28ad448c55b01bad4f7f84043aeaffd0
peace1nmind <aont95@naver.com>
2024-11-29 16:41:07 +0900

    test: CommunityService, LikeService 테스트

--- src/test/java/com/placeHere/server/StoreServiceTest.java
+++ src/test/java/com/placeHere/server/StoreServiceTest.java
@@ -3,6 +3,7 @@ package com.placeHere.server;
 import com.placeHere.server.dao.store.StoreDao;
 import com.placeHere.server.domain.*;
 import com.placeHere.server.service.community.CommunityService;
+import com.placeHere.server.service.like.LikeService;
 import com.placeHere.server.service.store.StoreService;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
@@ -28,6 +29,10 @@ public class StoreServiceTest {
     private CommunityService communityService;
 
     @Autowired
+    @Qualifier("likeServiceImpl")
+    private LikeService likeService;
+
+    @Autowired
     private StoreDao storeDao;
 
     @Value("${list_size}")
@@ -49,7 +54,20 @@ public class StoreServiceTest {
         System.out.println(reviewList.size());
         System.out.println(reviewList);
 
+    }
 
+    @Test
+    public void getlikeList() throws Exception {
+
+//        List<Integer> likeList = likeService.likeList("store");
+//
+//        System.out.println(likeList.size());
+//        System.out.println(likeList);
+
+        List<Like> storeLikeList = likeService.getStoreLikeList("user01");
+
+        System.out.println(storeLikeList.size());
+        System.out.println(storeLikeList);
 
     }
 
