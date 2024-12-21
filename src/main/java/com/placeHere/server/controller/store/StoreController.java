0ada39f7f9b2cc8de074f300c7414504d9da9536
peace1nmind <aont95@naver.com>
2024-12-21 12:01:10 +0900

    fix: AwsS3Service 인터페이스화

--- src/main/java/com/placeHere/server/controller/store/StoreController.java
+++ src/main/java/com/placeHere/server/controller/store/StoreController.java
@@ -1,7 +1,6 @@
 package com.placeHere.server.controller.store;
 
 import com.placeHere.server.domain.*;
-import com.placeHere.server.service.aws.AwsS3Service;
 import com.placeHere.server.service.community.CommunityService;
 import com.placeHere.server.service.like.LikeService;
 import com.placeHere.server.service.reservation.ReservationService;
@@ -14,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.Date;
