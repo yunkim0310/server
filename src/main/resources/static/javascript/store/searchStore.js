d1b9ecea385a3077dc0b5040bcc82642d6003f0e
peace1nmind <aont95@naver.com>
2024-12-26 21:19:21 +0900

    fix: 가게관리 - 검색 필터 오류 해결

--- src/main/resources/static/javascript/store/searchStore.js
+++ src/main/resources/static/javascript/store/searchStore.js
@@ -41,21 +41,10 @@ $(function () {
         $("input[name='mode']:hidden").val("result");
         $("input[name='page']").val(page);
 
-        const priceMin = $("input[name='priceMin']").val();
-        const priceMax = $("input[name='priceMax']").val();
-
-        if (priceMin === null || priceMin === undefined ||
-            priceMax === null || priceMax === undefined ||
-            priceMin > priceMax) {
-
-            alert("가격 범위 설정이 잘못되었습니다.");
-            $("input[name='priceMin']").focus();
-
-        } else {
-            $("form[name='searchStore']").attr("action", "/getStoreList").attr("method", "get").submit();
-        }
+        $("form[name='searchStore']").attr("action", "/getStoreList").attr("method", "get").submit();
 
     }
+
 
     // 검색 버튼
     $("button[name='searchStore']").on("click", function () {
